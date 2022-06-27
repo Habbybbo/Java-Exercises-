39410117f0c8b9fda05ef4c410bd75fc32547991
Yuliyan Andreev <habybbo@gmail.com>
2022-06-27 15:54:39 +0300

    Console printing
    My first steps in Java

---
+++ FirstStepsInJava/src/InitialSteps/ConsolePrinting.java
@@ -0,0 +1,9 @@
+package InitialSteps;
+
+public class ConsolePrinting {
+    public static void main (String [] args){
+        System.out.print("Hi, there!");
+        System.out.println(" How you've been?");
+        System.out.println("Haven't seen you in a while.");
+    }
+}
